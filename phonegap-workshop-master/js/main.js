2eeb175c231e088c3831bb1d410a75c3c99ba825
yossi-neeman <yossi.neeman@gmail.com>
2013-10-21 17:05:20 +0300

    Add phoneGap Alert

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -13,8 +13,19 @@ var app = {
         });
     },
 
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
     initialize: function() {
-        this.store = new MemoryStore();
+        var self = this;
+        this.store = new MemoryStore( function() {self.showAlert('Store Initialized', 'Info');
+        });
+
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
